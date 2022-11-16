023763f31781450520d215c54bd447e3ffc81d68
Tisfeng <tisfeng@gmail.com>
2022-11-16 13:33:49 +0800

    perf: style code, remove arrow rotation

--- OpenBob/Feature/TranslateWindow/Easydict/View/EZResultView/EZResultView.m
+++ OpenBob/Feature/TranslateWindow/Easydict/View/EZResultView/EZResultView.m
@@ -39,13 +39,13 @@
 - (void)setup {
     self.wantsLayer = YES;
     self.layer.cornerRadius = EZCornerRadius_8;
-
+    
     [self.layer excuteLight:^(CALayer *layer) {
         layer.backgroundColor = NSColor.resultViewBgLightColor.CGColor;
     } drak:^(CALayer *layer) {
         layer.backgroundColor = NSColor.resultViewBgDarkColor.CGColor;
     }];
-
+    
     self.topBarView = [NSView mm_make:^(NSView *_Nonnull view) {
         [self addSubview:view];
         view.wantsLayer = YES;
@@ -56,13 +56,13 @@
         }];
     }];
     self.topBarView.mas_key = @"topBarView";
-
+    
     self.typeImageView = [NSImageView mm_make:^(NSImageView *imageView) {
         [self addSubview:imageView];
         [imageView setImage:[NSImage imageNamed:@"Apple Translate"]];
     }];
     self.typeImageView.mas_key = @"typeImageView";
-
+    
     self.typeLabel = [NSTextField mm_make:^(NSTextField *label) {
         [self addSubview:label];
         label.editable = NO;
@@ -71,7 +71,7 @@
         label.alignment = NSTextAlignmentCenter;
         NSString *title = @"系统翻译";
         label.attributedStringValue = [[NSAttributedString alloc] initWithString:title];
-
+        
         [label excuteLight:^(NSTextField *label) {
             label.textColor = NSColor.resultTextLightColor;
         } drak:^(NSTextField *label) {
@@ -79,19 +79,19 @@
         }];
     }];
     self.typeLabel.mas_key = @"typeLabel";
-
+    
     self.disableImageView = [NSImageView mm_make:^(NSImageView *imageView) {
         [self addSubview:imageView];
         NSImage *image = [NSImage imageNamed:@"disabled"];
         [imageView setImage:image];
     }];
     self.disableImageView.mas_key = @"disableImageView";
-
-
+    
+    
     EZWordResultView *wordResultView = [[EZWordResultView alloc] initWithFrame:self.bounds];
     [self addSubview:wordResultView];
     self.wordResultView = wordResultView;
-
+    
     mm_weakify(self);
     [wordResultView setPlayAudioBlock:^(EZWordResultView *_Nonnull view, NSString *_Nonnull word) {
         mm_strongify(self);
@@ -99,35 +99,35 @@
             self.playAudioBlock(word);
         }
     }];
-
+    
     [wordResultView setCopyTextBlock:^(EZWordResultView *_Nonnull view, NSString *_Nonnull word) {
         if (self.copyTextBlock) {
             self.copyTextBlock(word);
         }
     }];
-
+    
     EZHoverButton *arrowButton = [[EZHoverButton alloc] init];
     self.arrowButton = arrowButton;
     [self addSubview:arrowButton];
     NSImage *image = [NSImage imageNamed:@"arrow-down"];
     arrowButton.image = image;
     self.arrowButton.mas_key = @"arrowButton";
-
+    
     [arrowButton setClickBlock:^(EZButton *_Nonnull button) {
         mm_strongify(self);
-
+        
         BOOL oldIsShowing = self.result.isShowing;
         BOOL newIsShowing = !oldIsShowing;
         self.result.isShowing = newIsShowing;
         NSLog(@"点击 arrowButton, show: %@", @(newIsShowing));
-
-        //        [self setNeedsUpdateConstraints:YES];
-
+        
+        [self setNeedsUpdateConstraints:YES];
+        
         if (self.clickArrowBlock) {
-            //            self.clickArrowBlock(newIsShowing);
+            self.clickArrowBlock(newIsShowing);
         }
-
-        [self rotateArrowButton];
+        
+        //        [self rotateArrowButton];
     }];
 }
 
@@ -149,43 +149,43 @@
 
 - (void)updateConstraints {
     CGSize iconSize = CGSizeMake(18, 18);
-
+    
     [self updateArrowButton];
-
+    
     [self.topBarView mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.top.left.right.equalTo(self);
         make.height.mas_equalTo(kResultViewMiniHeight);
     }];
-
+    
     [self.typeImageView mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.topBarView).offset(10);
         make.centerY.equalTo(self.topBarView);
         make.size.mas_equalTo(iconSize);
     }];
-
+    
     [self.typeLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.typeImageView.mas_right).offset(4);
         make.centerY.equalTo(self.topBarView).offset(0);
     }];
-
+    
     [self.disableImageView mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.typeLabel.mas_right).offset(5);
         make.centerY.equalTo(self.topBarView);
         make.size.mas_equalTo(iconSize);
     }];
-
+    
     [self.arrowButton mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.right.equalTo(self.topBarView.mas_right).offset(-5);
         make.centerY.equalTo(self.topBarView);
         make.size.mas_equalTo(CGSizeMake(22, 22));
     }];
-
+    
     [self.wordResultView mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.topBarView.mas_bottom);
         make.left.equalTo(self);
         make.bottom.right.lessThanOrEqualTo(self);
     }];
-
+    
     [super updateConstraints];
 }
 
@@ -196,16 +196,16 @@
 
 - (void)refreshWithResult:(TranslateResult *)result {
     _result = result;
-
+    
     EZServiceType serviceType = result.serviceType;
     NSString *imageName = [NSString stringWithFormat:@"%@ Translate", serviceType];
     self.typeImageView.image = [NSImage imageNamed:imageName];
-
+    
     TranslateService *translate = [ServiceTypes serviceWithType:serviceType];
     self.typeLabel.attributedStringValue = [NSAttributedString mm_attributedStringWithString:translate.name font:[NSFont systemFontOfSize:13]];
-
+    
     [self updateArrowButton];
-
+    
     [self.wordResultView refreshWithResult:result];
 }
 
