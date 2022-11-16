a44484bb5808e32127ae6a8c7196b9680a8d9e4c
Tisfeng <tisfeng@gmail.com>
2022-11-16 20:41:21 +0800

    perf: add another select text method

--- OpenBob/Feature/Selection/Selection.m
+++ OpenBob/Feature/Selection/Selection.m
@@ -22,7 +22,6 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         NSString *string = [[[[NSPasteboard generalPasteboard] pasteboardItems] firstObject] stringForType:NSPasteboardTypeString];
         NSLog(@"getText: %@", string);
-        
         completion(string);
     });
 }
