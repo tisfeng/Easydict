e302a93140eddf7232c1a7cf5c3211b7adf7dffb
Tisfeng <tisfeng@gmail.com>
2022-11-19 21:00:45 +0800

    perf: improve show window level, orderBack main window when show mini window

--- OpenBob/Feature/TranslateWindow/Easydict/Window/MiniQueryWindow/EZMiniQueryWindow.m
+++ OpenBob/Feature/TranslateWindow/Easydict/Window/MiniQueryWindow/EZMiniQueryWindow.m
@@ -13,38 +13,13 @@
 
 @implementation EZMiniQueryWindow
 
-
-- (instancetype)init {
-    NSWindowStyleMask style = NSWindowStyleMaskTitled;
-    
-    if (self = [super initWithContentRect:CGRectZero styleMask:style backing:NSBackingStoreBuffered defer:YES]) {
+- (instancetype)init {    
+    if (self = [super initWithContentRect:CGRectZero styleMask:NSWindowStyleMaskTitled backing:NSBackingStoreBuffered defer:YES]) {
         EZBaseQueryViewController *viewController = [[EZBaseQueryViewController alloc] init];
         self.viewController = viewController;
     }
     return self;
 }
-
-//- (instancetype)init {
-//    NSWindowStyleMask style = NSWindowStyleMaskTitled | NSWindowStyleMaskResizable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskClosable;
-//
-//    CGRect rect = CGRectMake(0, 0, EZMiniQueryWindowWidth, EZMiniQueryWindowWidth);
-//    if (self = [super initWithContentRect:rect styleMask:style backing:NSBackingStoreBuffered defer:YES]) {
-//        self.movableByWindowBackground = YES;
-//        self.level = NSModalPanelWindowLevel; // NSNormalWindowLevel
-//        self.titlebarAppearsTransparent = YES;
-//        self.titleVisibility = NSWindowTitleHidden;
-//
-//        [self excuteLight:^(NSWindow *window) {
-//            window.backgroundColor = NSColor.mainViewBgLightColor;
-//        } drak:^(NSWindow *window) {
-//            window.backgroundColor = NSColor.mainViewBgDarkColor;
-//        }];
-//
-//        EZBaseQueryViewController *miniVC = [[EZBaseQueryViewController alloc] init];
-//        self.contentViewController = miniVC;
-//    }
-//    return self;
-//}
 
 - (BOOL)canBecomeKeyWindow {
     return YES;
