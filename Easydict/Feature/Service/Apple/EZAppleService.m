15b66c00f6927439014362cc2c286529c9c391e2
Tisfeng <tisfeng@gmail.com>
2022-12-03 16:20:14 +0800

    perf: improve detection, correct alphabet to English

--- Easydict/Feature/Service/Apple/EZAppleService.m
+++ Easydict/Feature/Service/Apple/EZAppleService.m
@@ -85,18 +85,18 @@
 /// Apple ocr language: "en-US", "fr-FR", "it-IT", "de-DE", "es-ES", "pt-BR", "zh-Hans", "zh-Hant", "yue-Hans", "yue-Hant", "ko-KR", "ja-JP", "ru-RU", "uk-UA"
 - (MMOrderedDictionary *)ocrLanguageDictionary {
     MMOrderedDictionary *orderedDict = [[MMOrderedDictionary alloc] initWithKeysAndObjects:
-                                        EZLanguageSimplifiedChinese,  @"zh-Hans",
-                                        EZLanguageTraditionalChinese,  @"zh-Hant",
-                                        EZLanguageEnglish,  @"en-US",
-                                        EZLanguageJapanese,  @"ja-JP",
-                                        EZLanguageKorean,  @"ko-KR",
-                                        EZLanguageFrench,  @"fr-FR",
-                                        EZLanguageSpanish,  @"es-ES",
-                                        EZLanguagePortuguese,  @"pt-BR",
-                                        EZLanguageItalian,  @"it-IT",
-                                        EZLanguageGerman,  @"de-DE",
-                                        EZLanguageRussian,  @"ru-RU",
-                                        EZLanguageUkrainian,  @"uk-UA",
+                                        EZLanguageSimplifiedChinese, @"zh-Hans",
+                                        EZLanguageTraditionalChinese, @"zh-Hant",
+                                        EZLanguageEnglish, @"en-US",
+                                        EZLanguageJapanese, @"ja-JP",
+                                        EZLanguageKorean, @"ko-KR",
+                                        EZLanguageFrench, @"fr-FR",
+                                        EZLanguageSpanish, @"es-ES",
+                                        EZLanguagePortuguese, @"pt-BR",
+                                        EZLanguageItalian, @"it-IT",
+                                        EZLanguageGerman, @"de-DE",
+                                        EZLanguageRussian, @"ru-RU",
+                                        EZLanguageUkrainian, @"uk-UA",
                                         nil];
     return orderedDict;
 }
@@ -107,29 +107,34 @@
 /// Apple System language recognize.
 - (void)detect:(NSString *)text completion:(void (^)(EZLanguage, NSError *_Nullable))completion {
     // Ref: https://developer.apple.com/documentation/naturallanguage/identifying_the_language_in_text?language=objc
-
+    
     CFAbsoluteTime startTime = CFAbsoluteTimeGetCurrent();
-
+    
     // macos(10.14)
     NLLanguageRecognizer *recognizer = [[NLLanguageRecognizer alloc] init];
-
+    
     // Because Apple text recognition is often inaccurate, we need to limit the recognition language type.
     recognizer.languageConstraints = [self constraintLanguages];
     recognizer.languageHints = [self customLanguageHints];
-
+    
     [recognizer processString:text];
-
+    
     NSDictionary<NLLanguage, NSNumber *> *dict = [recognizer languageHypothesesWithMaximum:10];
     NSLog(@"language dict: %@", dict);
-
+    
     NLLanguage dominantLanguage = recognizer.dominantLanguage;
     NSLog(@"dominant Language: %@", dominantLanguage);
-
+    
     CFAbsoluteTime endTime = CFAbsoluteTimeGetCurrent();
     NSLog(@"detect cost: %.1f ms", (endTime - startTime) * 1000);
-
+    
     EZLanguage language = [self languageEnumFromString:dominantLanguage];
-
+    
+    if ([self isAlphabet:text]) {
+        language = EZLanguageEnglish;
+        NSLog(@"%@ isAlphabet, correct to English", text);
+    }
+    
     completion(language, nil);
 }
 
@@ -138,11 +143,11 @@
 - (void)ocr:(EZQueryModel *)queryModel completion:(void (^)(EZOCRResult *_Nullable ocrResult, NSError *_Nullable error))completion {
     // Convert NSImage to CGImage
     CGImageRef cgImage = [queryModel.image CGImageForProposedRect:NULL context:nil hints:nil];
-
+    
     CFAbsoluteTime startTime = CFAbsoluteTimeGetCurrent();
-
+    
     // Ref: https://developer.apple.com/documentation/vision/recognizing_text_in_images?language=objc
-
+    
     // Create a new image-request handler. macos(10.13)
     VNImageRequestHandler *requestHandler = [[VNImageRequestHandler alloc] initWithCGImage:cgImage options:@{}];
     // Create a new request to recognize text.
@@ -150,39 +155,39 @@
         VNRecognizeTextRequest *request = [[VNRecognizeTextRequest alloc] initWithCompletionHandler:^(VNRequest *_Nonnull request, NSError *_Nullable error) {
             CFAbsoluteTime endTime = CFAbsoluteTimeGetCurrent();
             NSLog(@"ocr cost: %.1f ms", (endTime - startTime) * 1000);
-
+            
             EZOCRResult *result = [[EZOCRResult alloc] init];
             result.from = queryModel.sourceLanguage;
             result.to = queryModel.targetLanguage;
-
+            
             if (error) {
                 completion(result, error);
                 return;
             }
-
+            
             NSMutableArray *recognizedStrings = [NSMutableArray array];
             for (VNRecognizedTextObservation *observation in request.results) {
                 VNRecognizedText *recognizedText = [[observation topCandidates:1] firstObject];
                 [recognizedStrings addObject:recognizedText.string];
             }
-
+            
             result.texts = recognizedStrings;
             result.mergedText = [recognizedStrings componentsJoinedByString:@"\n"];
             result.raw = recognizedStrings;
-
+            
             NSLog(@"ocr text: %@", recognizedStrings);
-
+            
             completion(result, nil);
         }];
-
-
+        
+        
         if (@available(macOS 12.0, *)) {
             //            NSError *error;
             //            NSArray<NSString *> *supportedLanguages = [request supportedRecognitionLanguagesAndReturnError:&error];
             // "en-US", "fr-FR", "it-IT", "de-DE", "es-ES", "pt-BR", "zh-Hans", "zh-Hant", "yue-Hans", "yue-Hant", "ko-KR", "ja-JP", "ru-RU", "uk-UA"
             //            NSLog(@"supported Languages: %@", supportedLanguages);
         }
-
+        
         NSMutableArray *recognitionLanguages = [NSMutableArray arrayWithArray:@[
             @"zh-Hans",
             @"zh-Hant",
@@ -199,7 +204,7 @@
             @"ru-RU",
             @"uk-UA",
         ]];
-
+        
         EZLanguage sourceLanguage = queryModel.sourceLanguage;
         if ([sourceLanguage isEqualToString:EZLanguageAuto]) {
             if (@available(macOS 13.0, *)) {
@@ -215,10 +220,10 @@
             }
         }
         request.recognitionLanguages = recognitionLanguages; // ISO language codes
-
+        
         // TODO: need to test it.
         request.usesLanguageCorrection = YES;
-
+        
         // Perform the text-recognition request.
         [requestHandler performRequests:@[ request ] error:nil];
     } else {
@@ -256,23 +261,34 @@
         NLLanguageKorean : @(0.4),
         NLLanguageGerman : @(0.2), // Bob 是一款 macOS 平台 翻译 和 OCR 软件
         NLLanguageTraditionalChinese : @(0.2),
-        NLLanguageItalian : @(0.1),    // via
-        NLLanguageSpanish : @(0.1),    // favor
-        NLLanguagePortuguese : @(0.1), // favor
-        NLLanguageDutch : @(0.1),      // heel, via
-
-        NLLanguageCzech : @(0.01), // pro
+        NLLanguageItalian : @(0.1), // via
+        NLLanguageSpanish : @(0.1), // favor
+        NLLanguageDutch : @(0.1),   // heel, via
+        
+        NLLanguagePortuguese : @(0.05), // favor, e
+        NLLanguageCzech : @(0.01),      // pro
     };
-
+    
     NSArray<NLLanguage> *allSupportedLanguages = [[self supportLanguagesDictionary] allValues];
     NSMutableDictionary<NLLanguage, NSNumber *> *languageHints = [NSMutableDictionary dictionary];
     for (NLLanguage language in allSupportedLanguages) {
         languageHints[language] = @(0.01);
     }
-
+    
     [languageHints addEntriesFromDictionary:customHints];
-
+    
     return languageHints;
+}
+
+/// Check if it is a single letter of the alphabet.
+- (BOOL)isAlphabet:(NSString *)string {
+    if (string.length != 1) {
+        return NO;
+    }
+    
+    NSString *regex = @"[a-zA-Z]";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [predicate evaluateWithObject:string];
 }
 
 @end
