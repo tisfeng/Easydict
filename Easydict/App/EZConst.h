b271a239fcf5756adef5f4c58ab51c5de4b47c11
tisfeng <tisfeng@gmail.com>
2022-12-30 22:33:46 +0800

    perf: improve updating result view animation

--- Easydict/App/EZConst.h
+++ Easydict/App/EZConst.h
@@ -19,9 +19,12 @@ static CGFloat const EZTitlebarHeight_28 = 28;
 static CGFloat const EZAudioButtonWidth_25 = 25;
 static CGFloat const EZAudioButtonImageWidth_15 = 15;
 
+static NSString *const EZBundleId = @"com.izual.Easydict";
 
 static NSString *const EZUserAgent = @"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36";
 
-static NSString *const EZBundleId = @"com.izual.Easydict";
+
+// ???: If value is not 0.2, it seems will block animation, because default animationResizeTime is 0.2 ?
+static NSTimeInterval const EZUpdateTableViewRowHeightAnimationDuration = 0.2;
 
 #endif /* EZConst_h */
