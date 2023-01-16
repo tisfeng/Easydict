3b7d249dd6d6c6770e46f1bd711ae527e70d9807
tisfeng <tisfeng@gmail.com>
2023-01-16 21:25:44 +0800

    feat: use Youdao web translate API instead of aidemo API

--- Easydict/Feature/MMKit/Category/EZCategory/NSString+EZConvenience.m
+++ Easydict/Feature/MMKit/Category/EZCategory/NSString+EZConvenience.m
@@ -7,6 +7,7 @@
 //
 
 #import "NSString+EZConvenience.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @implementation NSString (EZConvenience)
 
@@ -36,6 +37,22 @@
         return YES;
     }
     return NO;
+}
+
+- (NSString *)md5 {
+    const char *cStr = [self UTF8String];
+    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    CC_MD5(cStr, (CC_LONG)strlen(cStr), digest);
+#pragma clang diagnostic pop
+
+    NSMutableString *result = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [result appendFormat:@"%02x", digest[i]];
+    }
+    return result;
 }
 
 @end
