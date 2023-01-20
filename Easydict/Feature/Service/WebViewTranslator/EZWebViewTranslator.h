3b93ffd48c6623079f5326d0ea8465771738329c
tisfeng <tisfeng@gmail.com>
2023-01-20 11:16:27 +0800

    perf: enable webView customize delay retry count

--- Easydict/Feature/Service/WebViewTranslator/EZWebViewTranslator.h
+++ Easydict/Feature/Service/WebViewTranslator/EZWebViewTranslator.h
@@ -19,6 +19,8 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, copy) NSString *delayQuerySelector;
 @property (nonatomic, copy) NSString *delayJsCode;
 
+@property (nonatomic, assign) NSInteger delayRetryCount; // 10
+
 
 /// Preload url.
 - (void)preloadURL:(NSString *)URL;
