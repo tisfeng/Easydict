1c8f8ca4a5c1134c44fa12fbf15602e622f3c5ad
tisfeng <tisfeng@gmail.com>
2023-01-31 10:38:05 +0800

    perf: add query text length log

--- Easydict/Feature/MMKit/Log/EZLog/EZLog.m
+++ Easydict/Feature/MMKit/Log/EZLog/EZLog.m
@@ -22,20 +22,40 @@
 + (void)logService:(EZQueryService *)service {
     NSString *name = @"query_service";
     EZQueryModel *model = service.queryModel;
+    NSString *textLengthRange = [self textLengthRange:model.queryText];
     NSDictionary *dict = @{
         @"serviceType" : service.serviceType,
         @"queryType" : model.queryType,
         @"from" : model.queryFromLanguage,
         @"to" : model.queryTargetLanguage,
+        @"textLength" : textLengthRange,
     };
     [self logEventWithName:name parameters:dict];
+}
+
+/// Get text length range, 1-10, 10-50, 50-200, 200-1000, 1000-5000
++ (NSString *)textLengthRange:(NSString *)text {
+    NSInteger length = text.length;
+    if (length <= 10) {
+        return @"1-10";
+    } else if (length <= 50) {
+        return @"10-50";
+    } else if (length <= 200) {
+        return @"50-200";
+    } else if (length <= 1000) {
+        return @"200-1000";
+    } else if (length <= 5000) {
+        return @"1000-5000";
+    } else {
+        return @"5000+";
+    }
 }
 
 /// Log event.
 /// ⚠️ Event name must contain only letters, numbers, or underscores.
 /// ⚠️ parameters dict key and value both should be NSString.
 + (void)logEventWithName:(NSString *)name parameters:(nullable NSDictionary *)dict {
-//    NSLog(@"log event: %@, %@", name, dict);
+    //    NSLog(@"log event: %@, %@", name, dict);
     [MSACAnalytics trackEvent:name withProperties:dict];
     [FIRAnalytics logEventWithName:name parameters:dict];
 }
