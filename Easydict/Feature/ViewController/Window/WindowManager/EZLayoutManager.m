dbc4e6464c52c8df813092298dff4fc9f0eba11e
tisfeng <tisfeng@gmail.com>
2023-02-03 23:13:40 +0800

    feat(UI): add show fixed window postion option

--- Easydict/Feature/ViewController/Window/WindowManager/EZLayoutManager.m
+++ Easydict/Feature/ViewController/Window/WindowManager/EZLayoutManager.m
@@ -182,4 +182,13 @@ static EZLayoutManager *_instance;
     }
 }
 
+- (MMOrderedDictionary<NSNumber *, NSString *> *)fixedWindowPostionDict {
+    MMOrderedDictionary *dict = [[MMOrderedDictionary alloc] initWithKeysAndObjects:
+                                 @(EZFixedWindowPositionRight), NSLocalizedString(@"fixed_window_position_right", nil),
+                                 @(EZFixedWindowPositionMouse), NSLocalizedString(@"fixed_window_position_mouse", nil),
+                                 @(EZFixedWindowPositionFormer), NSLocalizedString(@"fixed_window_position_former", nil), nil];
+    
+    return dict;
+}
+
 @end
