7954542e12ab2f2f9b7f9f50442a975c09907fbf
tisfeng <tisfeng@gmail.com>
2023-02-23 15:50:53 +0800

    perf: add DeepL web translate API

---
+++ Easydict/Feature/Service/DeepL/EZQueryResult+EZDeepLTranslateResponse.m
@@ -0,0 +1,23 @@
+//
+//  EZQueryResult+EZDeepLTranslateResponse.m
+//  Easydict
+//
+//  Created by tisfeng on 2023/2/23.
+//  Copyright © 2023 izual. All rights reserved.
+//
+
+#import "EZQueryResult+EZDeepLTranslateResponse.h"
+
+@implementation EZQueryResult (EZDeepLTranslateResponse)
+
+- (instancetype)setupWithDeepLTranslateResponse:(EZDeepLTranslateResponse *)deepLTranslateResponse {
+    self.raw = deepLTranslateResponse;
+    NSString *firstResult = deepLTranslateResponse.result.texts.firstObject.text;
+    if (firstResult) {
+        self.normalResults = @[ firstResult ];
+    }
+
+    return self;
+}
+
+@end
