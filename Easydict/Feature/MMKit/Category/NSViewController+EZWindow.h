a6b3ed1bbaa3bd83a3d4086da8cb5c0444ef8980
tisfeng <tisfeng@gmail.com>
2023-04-19 17:43:57 +0800

    perf: improve hide menu bar icon alert

---
+++ Easydict/Feature/MMKit/Category/NSViewController+EZWindow.h
@@ -0,0 +1,19 @@
+//
+//  NSViewController+EZWindow.h
+//  Easydict
+//
+//  Created by tisfeng on 2023/4/19.
+//  Copyright © 2023 izual. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSViewController (EZWindow)
+
+- (NSWindow *)window;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Easydict/Feature/MMKit/Category/NSViewController+EZWindow.m
@@ -0,0 +1,23 @@
+//
+//  NSViewController+EZWindow.m
+//  Easydict
+//
+//  Created by tisfeng on 2023/4/19.
+//  Copyright © 2023 izual. All rights reserved.
+//
+
+#import "NSViewController+EZWindow.h"
+
+@implementation NSViewController (EZWindow)
+
+- (NSWindow *)window {
+    NSResponder *responder = self;
+    while ((responder = [responder nextResponder])) {
+        if ([responder isKindOfClass:[NSWindow class]]) {
+            return (NSWindow *)responder;
+        }
+    }
+    return nil;
+}
+
+@end
