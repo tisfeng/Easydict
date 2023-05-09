4129f341644bc7501e9e76820547b7f98a793506
tisfeng <tisfeng@gmail.com>
2023-05-09 11:09:19 +0800

    perf(UI): improve screenshot OCR toast

--- Easydict/Feature/MMKit/CoolToast/EZToast.m
+++ Easydict/Feature/MMKit/CoolToast/EZToast.m
@@ -32,5 +32,33 @@
     toast.messageLabel.alignment = NSTextAlignmentLeft; // must set after showCoolText
 }
 
++ (void)showText:(NSString *)message toastPostion:(CTPosition)toastPostion {
+    EZToast *toast = [EZToast toast];
+    toast.toastPostion = toastPostion;
+    toast.hiddenIcon = YES;
+    toast.imageMarginLeft = 0;
+    toast.labelMargin = 20;
+    toast.minHeight = 35;
+
+    [toast showCoolToast:message];
+    toast.messageLabel.alignment = NSTextAlignmentLeft; // must set after showCoolText
+}
+
++ (void)showSuccessToast {
+    EZToast *toast = [EZToast toast];
+    toast.toastPostion = CTPositionMouse;
+    toast.toastBackgroundColor = [NSColor mm_colorWithHexString:@"#CDCDCD"];
+    toast.textColor = [NSColor mm_colorWithHexString:@"#424242"];
+    toast.hiddenIcon = YES;
+    toast.imageMarginLeft = 0;
+    toast.labelMargin = 5;
+    toast.minHeight = 25;
+    toast.minWidth = 25;
+    toast.conerRadius = 5;
+    toast.messageLabel.font = [NSFont systemFontOfSize:18 weight:NSFontWeightBlack];
+
+    [toast showCoolToast:@"✓"]; // ✓  ✔︎
+    toast.messageLabel.alignment = NSTextAlignmentCenter; // must set after showCoolText
+}
 
 @end
