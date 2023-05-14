c07522ce9a4c898069320ea87af33f226fcd92d7
tisfeng <tisfeng@gmail.com>
2023-05-14 23:25:47 +0800

    perf: show latest version when a new version is available

--- Easydict/Feature/StatusItem/EZMenuItemManager.h
+++ Easydict/Feature/StatusItem/EZMenuItemManager.h
@@ -23,6 +23,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)remove;
 
+- (void)fetchRepoLatestRepoInfo:(NSString *)repo completion:(void (^)(NSDictionary *lastestVersionDict))completion;
+
 @end
 
 NS_ASSUME_NONNULL_END
