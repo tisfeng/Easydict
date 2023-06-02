c7e8d294978e498b4d8254aabc87ed29d0e30f67
tisfeng <tisfeng@gmail.com>
2023-06-02 11:34:34 +0800

    perf: improve OCR result text, join dash -

---
+++ Easydict/Feature/Utility/EZCategory/NSString/NSString+EZCharacterSet.h
@@ -0,0 +1,28 @@
+//
+//  NSString+EZCharacterSet.h
+//  Easydict
+//
+//  Created by tisfeng on 2023/6/2.
+//  Copyright © 2023 izual. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (EZCharacterSet)
+
+/// Check if it is a single letter of the alphabet, like 'a', 'A'
+- (BOOL)isAlphabet;
+
+- (BOOL)isAlphabeticString;
+
+/// Check if lowercaseString, like
+- (BOOL)isLowercaseString;
+
+/// Check if first char is lowercaseString
+- (BOOL)isLowercaseFirstChar;
+
+@end
+
+NS_ASSUME_NONNULL_END
