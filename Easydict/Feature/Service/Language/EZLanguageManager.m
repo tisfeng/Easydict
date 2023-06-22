b8c95c80c0c1962fd4baa1f03ffd0c9a5bc547f8
tisfeng <tisfeng@gmail.com>
2023-06-22 10:58:33 +0800

    perf: improve EZLanguageManager

--- Easydict/Feature/Service/Language/EZLanguageManager.m
+++ Easydict/Feature/Service/Language/EZLanguageManager.m
@@ -11,20 +11,55 @@
 
 @implementation EZLanguageManager
 
+static NSArray<EZLanguage> *_systemPreferredLanguages;
+
+// Get user system preferred languages
++ (NSArray<EZLanguage> *)systemPreferredLanguages {
+    if (!_systemPreferredLanguages) {
+        /**
+         "en-CN", "zh-Hans", "zh-Hans-CN"
+         ???: Why has changed to [ "zh-CN", "zh-Hans-CN", "en-CN" ]
+
+         [NSLocale preferredLanguages] is device languages, and it is read only.
+         [[NSUserDefaults standardUserDefaults] objectForKey:@"AppleLanguages"] is the same with [NSLocale preferredLanguages] generally, but it can be modified.
+         */
+
+        //  NSArray<NSString *> *preferredLanguages = [NSLocale preferredLanguages];
+        NSArray<NSString *> *preferredLanguages = [[NSUserDefaults standardUserDefaults] objectForKey:@"AppleLanguages"];
+        MMLogInfo(@"AppleLanguages: %@", preferredLanguages);
+
+        NSMutableArray *languages = [NSMutableArray array];
+        for (NSString *language in preferredLanguages) {
+            NSMutableArray *array = [NSMutableArray arrayWithArray:[language componentsSeparatedByString:@"-"]];
+            // Remove country code
+            [array removeLastObject];
+            NSString *languageCode = [array componentsJoinedByString:@"-"];
+            // Convert to EZLanguage
+            EZAppleService *appleService = [[EZAppleService alloc] init];
+            EZLanguage ezLanguage = [appleService languageEnumFromAppleLanguage:languageCode];
+
+            // handle "zh-CN"
+            if ([languageCode hasPrefix:@"zh"] && [ezLanguage isEqualToString:EZLanguageAuto]) {
+                ezLanguage = EZLanguageSimplifiedChinese;
+            }
+
+            if (![ezLanguage isEqualToString:EZLanguageAuto] && ![languages containsObject:ezLanguage]) {
+                [languages addObject:ezLanguage];
+            }
+        }
+
+        _systemPreferredLanguages = languages;
+
+        MMLogInfo(@"system preferred languages: %@", languages);
+    }
+
+    return _systemPreferredLanguages;
+}
+
+
 + (nullable EZLanguageModel *)languageModelFromLanguage:(EZLanguage)language {
     return [[EZLanguageModel allLanguagesDict] objectForKey:language];
 }
-
-// Get language model with locale identifier, such as "zh-CN"
-- (nullable EZLanguageModel *)languageModelFromLocaleIdentifier:(NSString *)localeIdentifier {
-    for (EZLanguageModel *languageModel in [EZLanguageModel allLanguagesDict].allValues) {
-        if ([languageModel.localeIdentifier isEqualToString:localeIdentifier]) {
-            return languageModel;
-        }
-    }
-    return nil;
-}
-
 
 // Get target language with source language
 + (EZLanguage)targetLanguageWithSourceLanguage:(EZLanguage)sourceLanguage {
@@ -37,52 +72,16 @@
     return targetLanguage;
 }
 
-// Get user system preferred languages
-+ (NSArray<EZLanguage> *)systemPreferredLanguages {
-    /**
-     "en-CN", "zh-Hans", "zh-Hans-CN"
-     ???: Why has changed to [ "zh-CN", "zh-Hans-CN", "en-CN" ]
-     
-     [NSLocale preferredLanguages] is device languages, and it is read only.
-     [[NSUserDefaults standardUserDefaults] objectForKey:@"AppleLanguages"] is the same with [NSLocale preferredLanguages] generally, but it can be modified.
-     */
-    
-//    NSArray<NSString *> *preferredLanguages = [NSLocale preferredLanguages];
-    NSArray<NSString *> *preferredLanguages = [[NSUserDefaults standardUserDefaults] objectForKey:@"AppleLanguages"];
-
-    NSMutableArray *languages = [NSMutableArray array];
-    for (NSString *language in preferredLanguages) {
-        NSMutableArray *array = [NSMutableArray arrayWithArray:[language componentsSeparatedByString:@"-"]];
-        // Remove country code
-        [array removeLastObject];
-        NSString *languageCode = [array componentsJoinedByString:@"-"];
-        // Convert to EZLanguage
-        EZAppleService *appleService = [[EZAppleService alloc] init];
-        EZLanguage ezLanguage = [appleService languageEnumFromAppleLanguage:languageCode];
-        
-        // handle "zh-CN"
-        if ([languageCode hasPrefix:@"zh"] && [ezLanguage isEqualToString:EZLanguageAuto]) {
-            ezLanguage = EZLanguageSimplifiedChinese;
-        }
-        
-        if (![ezLanguage isEqualToString:EZLanguageAuto] && ![languages containsObject:ezLanguage]) {
-            [languages addObject:ezLanguage];
-        }
-    }
-    
-    return languages;
-}
-
 + (NSArray<EZLanguage> *)preferredTwoLanguages {
     NSMutableArray *twoLanguages = [NSMutableArray array];
     NSMutableArray<EZLanguage> *preferredlanguages = [[self systemPreferredLanguages] mutableCopy];
-    
+
     EZLanguage firstLanguage = [self firstLanguageFromLanguages:preferredlanguages];
     [twoLanguages addObject:firstLanguage];
-    
+
     // Remove first language
     [preferredlanguages removeObject:firstLanguage];
-    
+
     EZLanguage secondLanguage = [self firstLanguageFromLanguages:preferredlanguages];
     if ([firstLanguage isEqualToString:secondLanguage]) {
         if ([firstLanguage isEqualToString:EZLanguageEnglish]) {
@@ -92,17 +91,16 @@
         }
     }
     [twoLanguages addObject:secondLanguage];
-    
+
     return twoLanguages;
 }
 
 // Get first language that is not auto, from languages
 + (EZLanguage)firstLanguageFromLanguages:(NSArray<EZLanguage> *)languages {
     for (EZLanguage language in languages) {
-        if ([language isEqualToString:EZLanguageAuto]) {
-            continue;
+        if (![language isEqualToString:EZLanguageAuto]) {
+            return language;
         }
-        return language;
     }
     return EZLanguageEnglish;
 }
@@ -217,7 +215,7 @@
 /// Showing language name according user first language, Chinese: English -> 英语, English: English -> English.
 + (NSString *)showingLanguageName:(EZLanguage)language {
     NSString *languageName = language ?: EZLanguageAuto;
-    
+
     if ([self isChineseFirstLanguage]) {
         languageName = [self languageChineseName:language];
     } else {
