36ac82fca4d99cb33cb102e8094f4b5a3cebed6b
tisfeng <tisfeng@gmail.com>
2023-10-24 22:33:26 +0800

    perf: improve tokenizer in testing detect word

--- EasydictTests/EasydictTests.m
+++ EasydictTests/EasydictTests.m
@@ -51,10 +51,9 @@
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     
-    NSString *text = @"si Il y a quatre-vingt-sept ans, nos pères ont donné naissance sur ce continent à une nouvelle nation, conçue dans la Liberté et vouée à la proposition selon laquelle tous les hommes sont créés égaux.\nMaintenant, nous sommes engagés dans une grande guerre civile, testant si cette nation, ou toute nation ainsi conçue et ainsi dévouée peut durer longtemps. Nous nous trouvons sur un grand champ de bataille de cette guerre. Nous sommes venus consacrer une partie de ce champ, comme lieu de repos final pour ceux qui ont donné leur vie ici pour que cette nation puisse vivre. Il est tout à fait approprié et approprié que nous fassions cela.\nMais, dans un sens plus large, nous ne pouvons pas consacrer – nous ne pouvons pas consacrer – nous ne pouvons pas sanctifier – ce terrain. Les hommes courageux, vivants et morts, qui ont lutté ici, l'ont consacré, bien au-dessus de notre pauvre pouvoir d'ajouter ou de retrancher. Le monde ne remarquera guère et ne se souviendra pas longtemps de ce que nous disons ici, mais il ne pourra jamais oublier ce qu’ils ont fait ici. C'est plutôt à nous, les vivants, de nous consacrer ici à l'œuvre inachevée que ceux qui ont combattu ici ont si noblement fait avancer jusqu'ici. C'est plutôt pour nous d'être ici consacrés à la grande tâche qui nous reste à accomplir – que de ces morts honorés nous tirions un dévouement accru à cette cause pour laquelle ils ont donné la dernière pleine mesure de dévouement – que nous sommes ici résolument résolus à ce que ces morts ne soient pas sont morts en vain – pour que cette nation, sous Dieu, connaisse une nouvelle naissance de liberté – et que le gouvernement du peuple, par le peuple, pour le peuple, ne périsse pas de la terre.";
+    NSString *text = @"Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.\n\nNow we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live. It is altogether fitting and proper that we should do this.\n\nBut, in a larger sense, we can not dedicate—we can not consecrate—we can not hallow—this ground. The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us—that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion—that we here highly resolve that these dead shall not have died in vain—that this nation, under God, shall have a new birth of freedom—and that government of the people, by the people, for the people, shall not perish from the earth.";
     
-    NSCharacterSet *separatorSet = [NSCharacterSet characterSetWithCharactersInString:@",.\n "];
-    NSArray *words = [text componentsSeparatedByCharactersInSet:separatorSet];
+    NSArray *words = [text wordsInText];
     
     for (NSString *word in words) {
         if (!word.length) {
@@ -62,12 +61,8 @@
         }
         
         EZLanguage language = [self.appleService detectText:word];
-        if ([language isEqualToString:EZLanguageEnglish]) {
-            if (![word isSpelledCorrectly]) {
-                NSLog(@"misspelled word: %@", word);
-            }
-        } else {
-            NSLog(@"Not English: %@ (%@)", word, language);
+        if (![language isEqualToString:EZLanguageEnglish]) {
+            NSLog(@"Detected language wrongly: %@ (%@)", word, language);
         }
     }
 }
