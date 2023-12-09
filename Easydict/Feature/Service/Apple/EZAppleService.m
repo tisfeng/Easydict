2a952aedd3dc5ce31f41a5d7278859b8a3dfb0a8
tisfeng <tisfeng@gmail.com>
2023-12-09 17:51:10 +0800

    fix: 𝙘𝙝𝙚𝙖𝙥 was detected as Simplified Chinese

--- Easydict/Feature/Service/Apple/EZAppleService.m
+++ Easydict/Feature/Service/Apple/EZAppleService.m
@@ -581,11 +581,10 @@ static EZAppleService *_instance;
     NSDictionary<NLLanguage, NSNumber *> *languageProbabilityDict = [recognizer languageHypothesesWithMaximum:5];
     NLLanguage dominantLanguage = recognizer.dominantLanguage;
     
-    // !!!: Numbers will be return empty dict @{}: 729
+    // !!!: languageProbabilityDict will be an empty dict @{} when text is Numbers, such as 729
     if (languageProbabilityDict.count == 0) {
-        EZLanguage firstLanguage = EZConfiguration.shared.firstLanguage;
-        dominantLanguage = [self appleLanguageFromLanguageEnum:firstLanguage];
-        languageProbabilityDict = @{dominantLanguage : @(0)};
+        dominantLanguage = [self detectUnkownText:text];
+        languageProbabilityDict = @{ dominantLanguage: @(0) };
     }
     
     CFAbsoluteTime endTime = CFAbsoluteTimeGetCurrent();
@@ -597,6 +596,19 @@ static EZAppleService *_instance;
     }
     
     return languageProbabilityDict;
+}
+
+- (NLLanguage)detectUnkownText:(NSString *)text {
+    NLLanguage language = NLLanguageEnglish;
+    // 729
+    if ([text isNumbers]) {
+        EZLanguage firstLanguage = EZConfiguration.shared.firstLanguage;
+        language = [self appleLanguageFromLanguageEnum:firstLanguage];
+    }
+    
+    // 𝙘𝙝𝙚𝙖𝙥
+    
+    return language;
 }
 
 // designatedLanguages is supportLanguagesDictionary remove some languages
