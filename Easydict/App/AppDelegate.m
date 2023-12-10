9ebb834be2ffef16bc6b791c263b1aaab55ec61b
tisfeng <tisfeng@gmail.com>
2023-12-11 01:41:34 +0800

    perf: set feedURLString to localhost URL when debuging

--- Easydict/App/AppDelegate.m
+++ Easydict/App/AppDelegate.m
@@ -101,7 +101,7 @@
 - (NSString *)feedURLStringForUpdater:(SUUpdater *)updater {
     NSString *feedURLString = @"https://raw.githubusercontent.com/tisfeng/Easydict/main/appcast.xml";
 #if DEBUG
-    feedURLString = @"https://raw.githubusercontent.com/tisfeng/Easydict/dev/appcast.xml";
+    feedURLString = @"http://localhost:8000/appcast.xml";
 #endif
     return feedURLString;
 }
