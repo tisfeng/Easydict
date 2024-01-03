a2ce4b5419ae673cdfca8da4df1d6b0fbc9916d7
phlpsong <103433299+phlpsong@users.noreply.github.com>
2024-01-03 22:02:29 +0800

    bugfix: menu item alignment issue (#299)
    * fix: issue #298 menu item alignment issue
    
    * fix: update format
    
    * Update Easydict/Feature/StatusItem/EZMenuItemManager.m
    
    Co-authored-by: Tisfeng <tisfeng@gmail.com>
    
    ---------
    
    Co-authored-by: Tisfeng <tisfeng@gmail.com>

--- Easydict/Feature/StatusItem/EZMenuItemManager.m
+++ Easydict/Feature/StatusItem/EZMenuItemManager.m
@@ -326,15 +326,9 @@ static EZMenuItemManager *_instance;
     NSFont *font = [NSFont systemFontOfSize:[NSFont systemFontSize]];
     CGFloat fontLineHeight = (font.ascender + fabs(font.descender));
     CGFloat lineHeight = fontLineHeight * lineHeightRatio;
-    NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
-    style.minimumLineHeight = lineHeight;
-    style.maximumLineHeight = lineHeight;
-    CGFloat baselineOffset = (lineHeight - fontLineHeight) / 2;
-    
-    item.attributedTitle = [[NSAttributedString alloc] initWithString:item.title attributes:@{
-        NSParagraphStyleAttributeName: style,
-        NSBaselineOffsetAttributeName: @(baselineOffset)
-    }];
+    // Ref stackoverflow: https://stackoverflow.com/a/18034142/8378840 
+    NSImage *image = [[NSImage alloc] initWithSize:NSMakeSize(1, lineHeight)];
+    [item setImage:image];
 }
 
 - (void)increaseMenuItemsHeight:(NSArray<NSMenuItem *> *)itmes lineHeightRatio:(CGFloat)lineHeightRatio {
