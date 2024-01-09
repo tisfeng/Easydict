419f8518a8c76c12ec1249c0cc832e822029cb45
Sharker <1548742234@qq.com>
2024-01-09 15:52:49 +0800

    chore: merge error

--- Easydict/NewApp/View/SettingView/Tabs/View/Genearl/GeneralKeyHolderWrapper.swift
+++ Easydict/NewApp/View/SettingView/Tabs/View/Genearl/GeneralKeyHolderWrapper.swift
@@ -8,11 +8,14 @@
 
 import AppKit
 import KeyHolder
+import Magnet
 import SwiftUI
 
 struct GeneralKeyHolderWrapper: NSViewRepresentable {
+    private var keyHolderAction = GeneralKeyHolderAction()
     func makeNSView(context _: Context) -> some NSView {
         let recordView = RecordView(frame: CGRect.zero)
+        recordView.delegate = keyHolderAction
         recordView.tintColor = NSColor(red: 0.164, green: 0.517, blue: 0.823, alpha: 1)
         recordView.layer?.cornerRadius = 6.0
         recordView.layer?.masksToBounds = true
@@ -20,4 +23,18 @@ struct GeneralKeyHolderWrapper: NSViewRepresentable {
     }
 
     func updateNSView(_: NSViewType, context _: Context) {}
+}
+
+class GeneralKeyHolderAction: RecordViewDelegate {
+    func recordViewShouldBeginRecording(_: KeyHolder.RecordView) -> Bool {
+        true
+    }
+
+    func recordView(_: KeyHolder.RecordView, canRecordKeyCombo _: Magnet.KeyCombo) -> Bool {
+        true
+    }
+
+    func recordView(_: KeyHolder.RecordView, didChangeKeyCombo _: Magnet.KeyCombo?) {}
+
+    func recordViewDidEndRecording(_: KeyHolder.RecordView) {}
 }
