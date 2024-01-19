502a3468be5645fdcdd607232fa4655a8df529a5
phlpsong <philsongfei@gmail.com>
2024-01-19 11:18:54 +0800

    fix: bundle application name issue

--- Easydict/Feature/Utility/Swift/Bundle/Bundle+AppInfo.swift
+++ Easydict/Feature/Utility/Swift/Bundle/Bundle+AppInfo.swift
@@ -10,9 +10,9 @@ import Foundation
 
 extension Bundle {
     var applicationName: String {
-        if let displayName: String = infoDictionary?["CFBundleDisplayName"] as? String {
+        if let displayName: String = object(forInfoDictionaryKey: "CFBundleDisplayName") as? String {
             return displayName
-        } else if let name: String = infoDictionary?["CFBundleName"] as? String {
+        } else if let name: String = object(forInfoDictionaryKey: "CFBundleName") as? String {
             return name
         }
         if let executableURL {
