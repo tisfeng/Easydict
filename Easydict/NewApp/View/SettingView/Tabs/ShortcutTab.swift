c9a4cd58250d6da067ecfcaadd9f864914bcc3a2
Sharker <1548742234@qq.com>
2024-01-21 20:20:33 +0800

    feat: add shortcut tab page

---
+++ Easydict/NewApp/View/SettingView/Tabs/ShortcutTab.swift
@@ -0,0 +1,21 @@
+//
+//  ShortcutTab.swift
+//  Easydict
+//
+//  Created by Sharker on 2024/1/21.
+//  Copyright © 2024 izual. All rights reserved.
+//
+
+import SwiftUI
+
+@available(macOS 13, *)
+struct ShortcutTab: View {
+    var body: some View {
+        Text("xx")
+    }
+}
+
+@available(macOS 13, *)
+#Preview {
+    ShortcutTab()
+}
