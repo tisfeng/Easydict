5931c9b778c89347406a5c590c8c8ef5c2329b5d
tisfeng <tisfeng@gmail.com>
2024-02-08 23:57:34 +0800

    perf: improve validation request case

--- Easydict/NewApp/Utility/Protocol/ServiceSecretConfigreValidatable.swift
+++ Easydict/NewApp/Utility/Protocol/ServiceSecretConfigreValidatable.swift
@@ -19,6 +19,12 @@ extension ServiceSecretConfigreValidatable {
 extension QueryService: ServiceSecretConfigreValidatable {
     func validate(completion: @escaping (EZQueryResult, Error?) -> Void) {
         resetServiceResult()
-        translate("hello world!", from: .english, to: .simplifiedChinese, completion: completion)
+        /**
+         To reduce output text, save cost, a simple translation example is enough.
+         
+         1. use zh -> en to avoid analyze English sentence.
+         2. if Chinese text length > 5, it won't query dict.
+         */
+        translate("曾经沧海难为水", from: .simplifiedChinese, to: .english, completion: completion)
     }
 }
