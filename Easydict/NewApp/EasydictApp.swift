4e7fbbbd10c46f67b37460acc5c1a1f3358750ce
tisfeng <tisfeng@gmail.com>
2024-02-25 16:02:04 +0800

    perf: use EasydictApp.main() only if available macOS 13

--- Easydict/NewApp/EasydictApp.swift
+++ Easydict/NewApp/EasydictApp.swift
@@ -14,7 +14,7 @@ import SwiftUI
 enum EasydictCmpatibilityEntry {
     static func main() {
         parseArmguments()
-        if NewAppManager.shared.enable {
+        if #available(macOS 13, *), NewAppManager.shared.enable {
             EasydictApp.main()
         } else {
             _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
