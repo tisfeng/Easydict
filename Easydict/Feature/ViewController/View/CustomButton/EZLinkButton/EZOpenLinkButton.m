7a7c06c2823ea51de2793a98f3a3b8651bbbccb3
tisfeng <tisfeng@gmail.com>
2024-03-18 23:13:20 +0800

    perf: do not close pinned window when clicking link button

--- Easydict/Feature/ViewController/View/CustomButton/EZLinkButton/EZOpenLinkButton.m
+++ Easydict/Feature/ViewController/View/CustomButton/EZLinkButton/EZOpenLinkButton.m
@@ -99,7 +99,9 @@ static NSString *const EZQueryKey = @"{Query}";
     // !!!: when using openURL, URL must has scheme, like https://
     BOOL success = [[NSWorkspace sharedWorkspace] openURL:URL];
     if (success) {
-        if (EZWindowManager.shared.floatingWindowType != EZWindowTypeMain) {
+        EZBaseQueryWindow *window = EZWindowManager.shared.floatingWindow;
+        // When opening a link, do not automatically close main window, or a pinned window.
+        if (!(window.windowType == EZWindowTypeMain || window.isPin)) {
             [[EZWindowManager shared] closeFloatingWindow];
         }
     }
