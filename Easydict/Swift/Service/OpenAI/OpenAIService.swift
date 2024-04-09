e5eebceea2ccd0bbcec2d7860ed7ed84fa4603ba
Tisfeng <tisfeng@gmail.com>
2024-04-10 00:38:01 +0800

    fix: openai usage status does not work (#501)

--- Easydict/Swift/Service/OpenAI/OpenAIService.swift
+++ Easydict/Swift/Service/OpenAI/OpenAIService.swift
@@ -6,10 +6,18 @@
 //  Copyright © 2023 izual. All rights reserved.
 //
 
+import Defaults
 import Foundation
 
 // MARK: - OpenAIService
 
-@objcMembers
 @objc(EZOpenAIService)
-public class OpenAIService: BaseOpenAIService {}
+class OpenAIService: BaseOpenAIService {
+    public override func serviceUsageStatus() -> EZServiceUsageStatus {
+        // swiftlint:disable:next todo
+        // TODO: Later, we need to support all services to use usage status.
+        let usageStatus = Defaults[.openAIServiceUsageStatus]
+        guard let value = UInt(usageStatus.rawValue) else { return .default }
+        return EZServiceUsageStatus(rawValue: value) ?? .default
+    }
+}
