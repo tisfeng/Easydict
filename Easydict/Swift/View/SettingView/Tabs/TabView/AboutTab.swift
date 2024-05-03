a667fb1574f7ae38ea9d9b8119cd9cf206927760
Sharker <1548742234@qq.com>
2024-05-03 17:30:31 +0800

    pref: pref about view UI resize the hight

--- Easydict/Swift/View/SettingView/Tabs/TabView/AboutTab.swift
+++ Easydict/Swift/View/SettingView/Tabs/TabView/AboutTab.swift
@@ -25,53 +25,57 @@ struct AboutTab: View {
     // MARK: Internal
 
     var body: some View {
-        HStack(alignment: .center, spacing: 20) {
-            Image(.logo)
-                .resizable()
-                .frame(width: 100, height: 100)
-                .padding()
-                .shadow(color: .gray, radius: 1, x: 0, y: 0.8)
+        ScrollView {
+            HStack(alignment: .center, spacing: 20) {
+                Image(.logo)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding()
+                    .shadow(color: .gray, radius: 1, x: 0, y: 0.8)
 
-            VStack(alignment: .leading) {
                 VStack(alignment: .leading) {
-                    Text(appName)
-                        .font(.system(size: 26, weight: .semibold))
+                    VStack(alignment: .leading) {
+                        Text(appName)
+                            .font(.system(size: 26, weight: .semibold))
 
-                    Spacer()
-                        .frame(height: 3)
+                        Spacer()
+                            .frame(height: 3)
 
-                    Text("current_version \(version)")
-                        .font(.system(size: 14))
-                        .foregroundColor(.gray)
+                        Text("current_version \(version)")
+                            .font(.system(size: 14))
+                            .foregroundColor(.gray)
 
-                    Spacer()
-                        .frame(height: 20)
+                        Spacer()
+                            .frame(height: 20)
 
-                    Text(copyrightInfo)
-                        .font(.system(size: 11))
-                        .foregroundColor(.gray)
-                }
-                .padding(.vertical, 3)
-
-                HStack {
-                    Button {
-                        NSWorkspace.shared.open(URL(string: "https://github.com/tisfeng/Easydict")!)
-                    } label: {
-                        Label("github_link", systemImage: "star.fill")
-                            .frame(width: 120)
+                        Text(copyrightInfo)
+                            .font(.system(size: 11))
+                            .foregroundColor(.gray)
                     }
+                    .padding(.vertical, 3)
 
-                    Button {
-                        NSWorkspace.shared.open(URL(string: "https://github.com/tisfeng/Easydict/graphs/contributors")!)
-                    } label: {
-                        Label("contributor_link", systemImage: "person.3.sequence.fill")
-                            .frame(width: 120)
+                    HStack {
+                        Button {
+                            NSWorkspace.shared.open(URL(string: "https://github.com/tisfeng/Easydict")!)
+                        } label: {
+                            Label("github_link", systemImage: "star.fill")
+                                .frame(width: 120)
+                        }
+
+                        Button {
+                            NSWorkspace.shared
+                                .open(URL(string: "https://github.com/tisfeng/Easydict/graphs/contributors")!)
+                        } label: {
+                            Label("contributor_link", systemImage: "person.3.sequence.fill")
+                                .frame(width: 120)
+                        }
                     }
                 }
             }
+            .padding(.top, 20)
+            .frame(maxWidth: .infinity)
         }
-        .padding()
-        .frame(maxWidth: .infinity)
+        .scrollIndicators(.hidden)
     }
 
     // MARK: Private
