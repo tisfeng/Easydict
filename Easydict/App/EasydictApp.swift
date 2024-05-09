92611f4edb27f148d54a45dc55b181fa802ebee5
tisfeng <tisfeng@gmail.com>
2024-05-09 16:04:16 +0800

    fix: resolve warning

--- Easydict/App/EasydictApp.swift
+++ Easydict/App/EasydictApp.swift
@@ -73,7 +73,7 @@ struct EasydictApp: App {
                     }
                 }
             }
-            
+
             Settings {
                 SettingView().environmentObject(languageState).environment(
                     \.locale,
