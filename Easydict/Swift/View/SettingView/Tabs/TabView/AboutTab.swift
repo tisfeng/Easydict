3213c8bc87183f2d196c7e77e35dc1ffa96de6e2
Jerry <jerry.23011@icloud.com>
2024-06-04 01:11:08 +0800

    format: auto-formatted by swiftlint

--- Easydict/Swift/View/SettingView/Tabs/TabView/AboutTab.swift
+++ Easydict/Swift/View/SettingView/Tabs/TabView/AboutTab.swift
@@ -114,9 +114,11 @@ struct AboutTab: View {
     }
 
     private var copyrightInfo: String {
-        Bundle.main.localizedString(forKey: "NSHumanReadableCopyright",
-                                    value: "Copyright © 2023-2024 tisfeng. All rights reserved.",
-                                    table: "InfoPlist")
+        Bundle.main.localizedString(
+            forKey: "NSHumanReadableCopyright",
+            value: "Copyright © 2023-2024 tisfeng. All rights reserved.",
+            table: "InfoPlist"
+        )
     }
 }
 
