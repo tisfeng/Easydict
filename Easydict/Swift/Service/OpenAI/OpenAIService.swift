18725e2dbef78aa010ce017f00c0fad67dea52ba
tisfeng <tisfeng@gmail.com>
2024-06-30 22:32:54 +0800

    refactor: rename enum OpenAIModel and GeminiModel, update gpt3_5_turbo to gpt_3_5_turbo

--- Easydict/Swift/Service/OpenAI/OpenAIService.swift
+++ Easydict/Swift/Service/OpenAI/OpenAIService.swift
@@ -48,9 +48,9 @@ class OpenAIService: BaseOpenAIService {
 enum OpenAIModel: String, CaseIterable {
     // Docs: https://platform.openai.com/docs/models
 
-    case gpt3_5_turbo = "gpt-3.5-turbo" // Currently points to gpt-3.5-turbo-0125. Input: $0.50 | Output: $1.50  (16k)
-    case gpt4_turbo = "gpt-4-turbo" // Currently points to gpt-4-turbo-2024-04-09. Input: $10 | Output: $30  (128k)
-    case gpt4o = "gpt-4o" // Currently points to gpt-4o-2024-05-13. Input: $5 | Output: $15  (128k context length)
+    case gpt_3_5_turbo = "gpt-3.5-turbo" // Currently points to gpt-3.5-turbo-0125. Input: $0.50 | Output: $1.50  (16k)
+    case gpt_4_turbo = "gpt-4-turbo" // Currently points to gpt-4-turbo-2024-04-09. Input: $10 | Output: $30  (128k)
+    case gpt_4o = "gpt-4o" // Currently points to gpt-4o-2024-05-13. Input: $5 | Output: $15  (128k context length)
 }
 
 // swiftlint:enable identifier_name
