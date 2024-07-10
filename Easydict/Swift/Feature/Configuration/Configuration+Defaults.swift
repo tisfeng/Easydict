817a0739e5a31ed17082885b1574019461628c75
tisfeng <tisfeng@gmail.com>
2024-07-10 23:48:14 +0800

    Update Easydict/Swift/Feature/Configuration/Configuration+Defaults.swift

--- Easydict/Swift/Feature/Configuration/Configuration+Defaults.swift
+++ Easydict/Swift/Feature/Configuration/Configuration+Defaults.swift
@@ -96,7 +96,7 @@ extension Defaults.Keys {
     )
 
     static var disableTipsView = Key<Bool>("disableTipsViewKey", default: false)
-    static var enableYoudaoOCR = Key<Bool>("enableYoudaoOCR", default: true)
+    static var enableYoudaoOCR = Key<Bool>("enableYoudaoOCR", default: false)
 }
 
 extension Defaults.Keys {
