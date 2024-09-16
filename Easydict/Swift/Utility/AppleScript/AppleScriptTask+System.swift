ba56d09e4ffdb7366faa39b0b861850d7477ded2
tisfeng <tisfeng@gmail.com>
2024-09-16 22:40:22 +0800

    refactor: rewrite AppleScript with Swift (#675)
    * feat: add swift AppleScriptTask
    
    * refactor: replace EZAppleScriptManager with AppleScriptTask
    
    * fix: callback getSelectedTextFromBrowser in main queue
    
    * refactor: replace AppleScriptUtils with AppleScriptTask
    
    * refactor: replace EZScriptExecutor with AppleScriptTask
    
    * refactor: replace AppleScriptError with QueryError
    
    * style: rename to Easydict

---
+++ Easydict/Swift/Utility/AppleScript/AppleScriptTask+System.swift
@@ -0,0 +1,27 @@
+//
+//  AppleScriptTask+System.swift
+//  Easydict
+//
+//  Created by tisfeng on 2024/9/11.
+//  Copyright © 2024 izual. All rights reserved.
+//
+
+import Foundation
+
+extension AppleScriptTask {
+    static func getAlertVolume() async throws -> Int32 {
+        let script = "get alert volume of (get volume settings)"
+        if let volumeString = try await runAppleScript(script),
+           let volume = Int32(volumeString) {
+            logInfo("AppleScript get alert volume: \(volume)")
+            return volume
+        }
+        throw QueryError(type: .appleScript, message: "Failed to get alert volume")
+    }
+
+    static func setAlertVolume(_ volume: Int) async throws {
+        let script = "set volume alert volume \(volume)"
+        try await runAppleScript(script)
+        logInfo("AppleScript set alert volume: \(volume)")
+    }
+}
