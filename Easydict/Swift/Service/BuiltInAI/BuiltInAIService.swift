513c40c8f0d1c1c9e48aad7d2be4b9268743d523
tisfeng <tisfeng@gmail.com>
2025-01-20 22:32:46 +0800

    fix: update built-in models, remove hunyuan-lite

--- Easydict/Swift/Service/BuiltInAI/BuiltInAIService.swift
+++ Easydict/Swift/Service/BuiltInAI/BuiltInAIService.swift
@@ -45,17 +45,15 @@ class BuiltInAIService: BaseOpenAIService {
         [
             "glm-4-flash",
             // zhipu free model, 128k context, 4k output, 200 QoS https://bigmodel.cn/dev/howuse/model
-            "hunyuan-lite",
-            // tencent free model, 256k context, 6k output, 5 QoS https://cloud.tencent.com/document/product/1729/104753
 
             // Groq free models https://console.groq.com/docs/models
             "llama-3.1-8b-instant", // 8k context, 30 RPM, 14,400 RPD, 20,000 TPM, 500,000 TPD
-            "llama-3.1-70b-versatile", // 8k context, 30 RPM, 14,400 RPD, 6,000 TPM, 200,000 TPD
+            "llama-3.3-70b-versatile", // 128k context, 30 RPM, 14,400 RPD, 6,000 TPM, 200,000 TPD
             "llama3-70b-8192", // 8k context, 30 RPM, 14,400 RPD, 6,000 TPM, 500,000 TPD
             "gemma2-9b-it", // 8k context, 30 RPM, 14,400 RPD, 15,000 TPM, 500,000 TPD
 
-            // Google Gemini https://ai.google.dev/pricing?hl=zh-cn
-            "gemini-1.5-flash", // free, 15 RPM, 100,000 TPD, 1,000 TPM, 1,500,000 RPD
+            // Google Gemini https://ai.google.dev/gemini-api/docs/models/gemini
+            "gemini-1.5-flash", // Free: 15 RPM, 1,000,000 TPM, 1,500 RPD
         ]
     }
 
