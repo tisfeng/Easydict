96faef958fce74d7b3ccada8289cbdc4f292a6b4
tisfeng <tisfeng@gmail.com>
2025-01-21 22:02:59 +0800

    feat: add SiliconFlow free model yi-1.5-9b for built-in service

--- Easydict/Swift/Service/BuiltInAI/BuiltInAIService.swift
+++ Easydict/Swift/Service/BuiltInAI/BuiltInAIService.swift
@@ -43,14 +43,16 @@ class BuiltInAIService: BaseOpenAIService {
 
     override var defaultModels: [String] {
         [
-            "glm-4-flash",
+            // SiliconFlow free models https://cloud.siliconflow.cn/models
+            "yi-1.5-9b", // 01-ai/Yi-1.5-9B-Chat-16K, 1,000 RPM,  50,000 TPM
+
             // zhipu free model, 128k context, 4k output, 200 QoS https://bigmodel.cn/dev/howuse/model
+            "glm-4-flash",
 
             // Groq free models https://console.groq.com/docs/models
-            "llama-3.1-8b-instant", // 8k context, 30 RPM, 14,400 RPD, 20,000 TPM, 500,000 TPD
-            "llama-3.3-70b-versatile", // 128k context, 30 RPM, 14,400 RPD, 6,000 TPM, 200,000 TPD
-            "llama3-70b-8192", // 8k context, 30 RPM, 14,400 RPD, 6,000 TPM, 500,000 TPD
             "gemma2-9b-it", // 8k context, 30 RPM, 14,400 RPD, 15,000 TPM, 500,000 TPD
+            "llama3-70b-8192", // 8k context, 30 RPM, 14,400 RPD, 6,000 TPM, 500,000 TPD
+            "llama-3.1-70b-versatile", // 128k context, 30 RPM, 14,400 RPD, 6,000 TPM, 200,000 TPD
 
             // Google Gemini https://ai.google.dev/gemini-api/docs/models/gemini
             "gemini-1.5-flash", // Free: 15 RPM, 1,000,000 TPM, 1,500 RPD
