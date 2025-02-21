07b938a9014834d66d8d6a0c1ef238e6fe22b117
tisfeng <tisfeng@gmail.com>
2025-02-21 22:46:40 +0800

    fix: update built-in models

--- Easydict/Swift/Service/BuiltInAI/BuiltInAIService.swift
+++ Easydict/Swift/Service/BuiltInAI/BuiltInAIService.swift
@@ -43,20 +43,14 @@ class BuiltInAIService: BaseOpenAIService {
 
     override var defaultModels: [String] {
         [
-            // SiliconFlow free models https://cloud.siliconflow.cn/models
-            "yi-1.5-9b", // 01-ai/Yi-1.5-9B-Chat-16K, 1,000 RPM,  50,000 TPM
-
-            // zhipu free model https://bigmodel.cn/dev/howuse/model
-            "glm-4-flash", // glm-4-flash, 128k context, 4k output, 200 QoS | THUDM/glm-4-9b-chat(SiliconFlow)
-
             // Groq free models https://console.groq.com/docs/models
-            "gemma-2-9b-it",
-            // gemma2-9b-it, 8k, 30 RPM, 14,400 RPD, 15,000 TPM, 500,000 TPD | google/gemma-2-9b-it(SiliconFlow)
+            "gemma-2-9b", // gemma2-9b-it, 8k, 30 RPM, 14,400 RPD, 15,000 TPM, 500,000 TPD
+            "llama-3.1-8b", // llama-3.1-8b-instant, 128k, 30 RPM, 14,400 RPD, 6,000 TPM, 500,000 TPD
             "llama-3-70b", // llama3-70b-8192, 8k, 30 RPM, 14,400 RPD, 6,000 TPM, 500,000 TPD
             "llama-3.3-70b", // llama-3.3-70b-versatile, 128k, 30 RPM, 1,00 RPD, 6,000 TPM, 100,000 TPD
 
-            // Google Gemini https://ai.google.dev/gemini-api/docs/models/gemini
-            "gemini-1.5-flash", // Free: 15 RPM, 1,000,000 TPM, 1,500 RPD
+            // zhipu free model https://bigmodel.cn/dev/howuse/model
+            "glm-4-flash", // glm-4-flash, 128k context, 4k output, 200 QoS | THUDM/glm-4-9b-chat(SiliconFlow)
         ]
     }
 
