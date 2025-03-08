a300e55b8abaec01d56a5bf0803414f6278eaa18
tisfeng <tisfeng@gmail.com>
2025-03-08 16:17:28 +0800

    perf: update built-in models

--- Easydict/Swift/Service/BuiltInAI/BuiltInAIService.swift
+++ Easydict/Swift/Service/BuiltInAI/BuiltInAIService.swift
@@ -43,11 +43,17 @@ class BuiltInAIService: BaseOpenAIService {
 
     override var defaultModels: [String] {
         [
+            // Mistral free models https://docs.mistral.ai/getting-started/models/models_overview/#free-models
+            "mistral-small-latest", // 32k, 1 RPS, 500,000 TPM,  [≈ Llama 3.3 70B or Qwen 32B]
+
             // Groq free models https://console.groq.com/docs/models
-            "gemma-2-9b", // gemma2-9b-it, 8k, 30 RPM, 14,400 RPD, 15,000 TPM, 500,000 TPD
-            "llama-3-70b", // llama3-70b-8192, 8k, 30 RPM, 14,400 RPD, 6,000 TPM, 500,000 TPD
-            "llama-3.3-70b", // llama-3.3-70b-versatile, 128k, 30 RPM, 1,00 RPD, 6,000 TPM, 100,000 TPD
-            "qwen-2.5-32b", // qwen-2.5-32b, 128k, 30 RPM, 1,000 RPD, 6,000 TPM,
+            "gemma-2-9b", // gemma2-9b-it, 30 RPM, 14,400 RPD, 15,000 TPM, 500,000 TPD
+            "llama-3-70b", // llama3-70b-8192, 30 RPM, 14,400 RPD, 6,000 TPM, 500,000 TPD
+            "llama-3.3-70b", // llama-3.3-70b-versatile, 30 RPM, 1,00 RPD, 6,000 TPM, 100,000 TPD
+            "qwen-2.5-32b", // 30 RPM, 1,000 RPD, 6,000 TPM,
+
+            // Google free models https://ai.google.dev/gemini-api/docs/models/gemini#gemini-2.0-flash-lite
+            "gemini-2.0-flash-lite", // 30 RPM, 1,500 RPD, 1,000,000 TPM
 
             // zhipu free model https://bigmodel.cn/dev/howuse/model
             "glm-4-flash", // glm-4-flash, 128k context, 4k output, 200 QoS | THUDM/glm-4-9b-chat(SiliconFlow)
