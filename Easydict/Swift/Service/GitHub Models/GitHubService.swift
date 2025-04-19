9d5526d88566b7847d0e157478921a2368e9392f
tisfeng <tisfeng@gmail.com>
2025-04-19 11:01:30 +0800

    fix: update github models, remove prefix like deepseek/

--- Easydict/Swift/Service/GitHub Models/GitHubService.swift
+++ Easydict/Swift/Service/GitHub Models/GitHubService.swift
@@ -57,21 +57,27 @@ enum GitHubModel: String, CaseIterable {
     case gpt_4_1_mini = "gpt-4.1-mini" // Low: 15 RPM | 150 RPD
     case gpt_4_1_nano = "gpt-4.1-nano" // Low
 
-    case o3 // Custom: 2 RPM | 12 RPD
-    case o4_mini = "o4-mini" // Custom
+    /**
+     o-series models are not good for translation, since they are expensive and slow,
+     and can only use temperature 1.0 which is not consistent with the other models.
+
+     case o3 // Custom: 2 RPM | 12 RPD
+     case o4_mini = "o4-mini" // Custom
+     */
 
     case gpt_4o = "gpt-4o" // High
     case gpt_4o_mini = "gpt-4o-mini" // Low
 
-    case deepseek_v3_0324 = "deepseek/DeepSeek-V3-0324" // High
+    case deepseek_v3_0324 = "deepseek-v3-0324" // High
 
-    case llama_3_3_70B_instruct = "Llama-3.3-70B-Instruct" // High
+    case llama_4_maverick_17b_128e_instruct_fp8 = "llama-4-maverick-17b-128e-instruct-fp8" // High
 
-    case cohere_command_a = "cohere/cohere-command-a" // Low
+    case cohere_command_a = "cohere-command-a" // Low
 
-    case mistral_small_2503 = "mistral-ai/mistral-small-2503" // Low
+    // Mistral Small 3.1
+    case mistral_small_2503 = "mistral-small-2503" // Low
 
-    case phi_4 = "Phi-4" // Low
+    case phi_4 = "phi-4" // Low
 }
 
 // swiftlint:enable identifier_name
