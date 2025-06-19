fe8c1b068ac0ebef3f8aa1584f58d7cf878a39ea
tisfeng <tisfeng@gmail.com>
2025-06-19 22:32:55 +0800

    fix: rename modifyLanduage to modifyAppLanguage
    Signed-off-by: tisfeng <tisfeng@gmail.com>

--- Easydict/objc/ViewController/Window/BaseQueryWindow/EZBaseQueryViewController.m
+++ Easydict/objc/ViewController/Window/BaseQueryWindow/EZBaseQueryViewController.m
@@ -204,7 +204,7 @@ static void dispatch_block_on_main_safely(dispatch_block_t block) {
     }];
 
     [defaultCenter addObserver:self
-                      selector:@selector(modifyLanduage:)
+                      selector:@selector(modifyAppLanguage:)
                           name:NSNotification.languagePreferenceChanged
                         object:nil];
 
@@ -254,7 +254,7 @@ static void dispatch_block_on_main_safely(dispatch_block_t block) {
     [self reloadTableViewData:nil];
 }
 
-- (void)modifyLanduage:(NSNotification *)notification {
+- (void)modifyAppLanguage:(NSNotification *)notification {
     [self.tableView reloadData];
 }
 
